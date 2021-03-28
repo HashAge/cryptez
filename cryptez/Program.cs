bf3f3b8f664df9812e88e9f296e143de169adea5
HashAge <aleshka.eagle@bk.ru>
2021-03-28 13:38:40 +0300

    Added function:base32 encryption

--- cryptez/Program.cs
+++ cryptez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Text;
+using System.Collections.Generic;
 namespace cryptez
 {
     class MainClass
@@ -8,10 +9,16 @@ namespace cryptez
         {
             lib_of_cpt lib = new lib_of_cpt();
 
-            Console.WriteLine("Cryptez");
+            Console.WriteLine("   ___               _          ");
+            Console.WriteLine("  / __|_ _ _  _ _ __| |_ ___ ___");
+            Console.WriteLine(" | (__| '_| || | '_ \\  _/ -_)_ /");
+            Console.WriteLine("  \\___|_|  \\_, | .__/\\__\\___/__|");
+            Console.WriteLine("           |__/|_|              ");
             Console.WriteLine("1.Ceaser cipher");
-            Console.WriteLine("2.Vigenere vipher\n");
+            Console.WriteLine("2.Vigenere vipher");
+            Console.WriteLine("3.Base64");
             string ch = Console.ReadLine();
+            if (Int32.TryParse(ch, out int a) == false) return;
             Console.Clear();
             lib.choice(Int32.Parse(ch));
 
@@ -45,7 +52,11 @@ namespace cryptez
                     string des = Console.ReadLine();
                     vigenere(mes, code, Int32.Parse(des));
                 }
-                else return;
+                else if (choice == 3) {
+                    Console.WriteLine("Enter the plain text");
+                    string mes = Console.ReadLine();
+                    b_ase64(mes);
+                };
             }
             public void vigenere(string message, string code,int choice)
             {
@@ -95,7 +106,9 @@ namespace cryptez
                     }
                     Console.WriteLine(mes_char);
                 }
+                Console.Read();
             }
+            //*************************************************************************************************
             public void cesaer(string message, int code, int choice)
             {
                 if(choice == 1)
@@ -128,6 +141,34 @@ namespace cryptez
                     }
                     Console.WriteLine(chr);
                 }
+
+                Console.Read();
+            //*************************************************************************************************    
+            }
+            public void b_ase64(string mes)
+            {
+                int i = 0;               
+                byte[] mes_byte = Encoding.ASCII.GetBytes(mes);
+                string bin = "";
+                string map = "abcdefghijklmnopqrstuvwxyz234567";
+                string temp = "";
+                string fin = "";
+                //converting message to binary
+                foreach (byte b in mes_byte) {                    
+                    bin += Convert.ToString(mes_byte[i], 2);
+                    i++;
+                }
+                //converting binary to base32
+                for (int b = 0; b < bin.Length; b++)
+                {
+                    temp += bin[b];
+                    if (b % 5 == 0 || b == bin.Length) {
+                        fin+=map[Convert.ToInt32(temp,2)];
+                        temp = "";                   
+                    };
+                }
+                Console.WriteLine(fin);
+                Console.ReadLine();
             }
         }
     }
